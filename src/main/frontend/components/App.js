1ca008a8efdec536bde762ff9b1672a84e15983a
ikrisa10 <ilyakrisa@gmail.com>
2021-05-24 11:33:54 -0700

    first commit

---
+++ src/main/frontend/components/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { hot } from "react-hot-loader/root"
+
+
+const App = props => {
+  return (
+    <div>Hello From React</div>
+  )
+}
+export default hot(App)
---
+++ src/main/resources/db/migration/V1__album_table.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS albums CASCADE;
+CREATE TABLE albums (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  artist VARCHAR(255) NOT NULL,
+  genre VARCHAR(255) NOT NULL,
+  cover_url VARCHAR(500),
+  release_year INTEGER NOT NULL,
+  songs TEXT NOT NULL
+)
