6e68c8ddf28b75892d8f3aa32a0f631814b7344e
ikrisa10 <ilyakrisa@gmail.com>
2021-05-24 15:51:07 -0700

    schema updates and created packages

--- src/main/resources/db/migration/V1__album_table.sql
+++ src/main/resources/db/migration/V1__album_table.sql
@@ -4,7 +4,8 @@ CREATE TABLE albums (
   title VARCHAR(255) NOT NULL,
   artist VARCHAR(255) NOT NULL,
   genre VARCHAR(255) NOT NULL,
+  email VARCHAR(320) NOT NULL,
   cover_url VARCHAR(500),
   release_year INTEGER NOT NULL,
-  songs TEXT NOT NULL
+  embed_url TEXT NOT NULL
 )
