dcac5c199b8b4d9ae1545a7e53a7c9981664c52c
PatrickStevenson <78173961+PatrickStevenson@users.noreply.github.com>
2021-05-25 14:22:06 -0700

    created endpoints for get mapping

--- src/main/java/com/launchacademy/reviews/controllers/ApiRestControllerV1.java
+++ src/main/java/com/launchacademy/reviews/controllers/ApiRestControllerV1.java
@@ -1,13 +1,19 @@
 package com.launchacademy.reviews.controllers;
 
+import com.launchacademy.reviews.models.Album;
+import com.launchacademy.reviews.models.Review;
 import com.launchacademy.reviews.services.AlbumService;
 import com.launchacademy.reviews.services.ReviewsService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/albums")
+@RequestMapping("/api/v1")
 public class ApiRestControllerV1 {
   private AlbumService albumService;
   private ReviewsService reviewsService;
@@ -18,4 +24,25 @@ public class ApiRestControllerV1 {
     this.albumService = albumService;
     this.reviewsService = reviewsService;
   }
+
+  @GetMapping("/albums")
+  public List<Album> getAllAlbums() {
+    return albumService.findAllAlbums();
+  }
+
+  @GetMapping("/albums/random")
+  public Album getRandomAlbum() {
+    return albumService.getRandomAlbum();
+  }
+
+  @GetMapping("/reviews/{albumId}")
+  public List<Review> getAllReviewsForAlbum(@PathVariable Integer albumId) {
+    return reviewsService.findAllReviewsForAlbum(albumId);
+  }
+
+  @GetMapping("/albums/undiscovered")
+  public List<Album> getUnreviewedAlbums() {
+    return albumService.findUnreviewedAlbums();
+  }
+
 }
--- src/main/java/com/launchacademy/reviews/services/ReviewsService.java
+++ src/main/java/com/launchacademy/reviews/services/ReviewsService.java
@@ -1,15 +1,27 @@
 package com.launchacademy.reviews.services;
 
+import com.launchacademy.reviews.models.Album;
+import com.launchacademy.reviews.models.Review;
+import com.launchacademy.reviews.repositories.AlbumsRepository;
 import com.launchacademy.reviews.repositories.ReviewsRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewsService {
   private ReviewsRepository reviewsRepository;
+  private AlbumService albumService;
 
   @Autowired
-  public ReviewsService(ReviewsRepository reviewsRepository) {
+  public ReviewsService(ReviewsRepository reviewsRepository, AlbumService albumService) {
     this.reviewsRepository = reviewsRepository;
+    this.albumService = albumService;
   }
+
+	public List<Review> findAllReviewsForAlbum(Integer albumId) {
+		Album targetAlbum = albumService.findById(albumId);
+		return targetAlbum.getReviews();
+	}
+
 }
