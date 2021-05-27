3876cb268f658ab2d4949ac614907851168f364d
kpartain <karalynnleann@gmail.com>
2021-05-27 15:03:16 -0700

    Added video background to landing

--- src/main/frontend/components/Index.js
+++ src/main/frontend/components/Index.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react"
+import React, {useState, useEffect} from "react"
 import AlbumTile from "./AlbumTile.js"
 
 const IndexPage = props => {
     const [albums, setAlbums] = useState([])
 
-    
+
     const getAlbums = async () => {
         try {
             const response = await fetch(`/api/v1/albums`)
@@ -19,7 +19,7 @@ const IndexPage = props => {
             console.error(`Error in Fetch: ${err.message}`)
         }
     }
-    
+
     useEffect(() => {
         getAlbums()
     }, [])
@@ -38,14 +38,14 @@ const IndexPage = props => {
 
     return (
         <div className="centered">
-        <div className="grid-container">
-            <h1 id="head">All Albums to Review</h1>
-            <div className="grid-x grid-padding-x">
-            <ul className="grid-x grid-margin-x" id="album">
-            {albumList}
-            </ul>
-        </div>
-        </div>
+            <div className="grid-container">
+                <h1 id="head">All Albums to Review</h1>
+                <div className="grid-x grid-padding-x">
+                    <ul className="grid-x grid-margin-x" id="album">
+                        {albumList}
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
--- src/main/frontend/components/Landing.js
+++ src/main/frontend/components/Landing.js
@@ -1,66 +1,69 @@
-import React, { useState, useEffect } from "react";
-import { Route, Switch, Link } from "react-router-dom";
+import React, {useState, useEffect} from "react";
+import {Route, Switch, Link} from "react-router-dom";
 import AlbumTile from "./AlbumTile.js";
 import ReviewTile from "./ReviewTile";
 
 const Landing = (props) => {
-  const [randomAlbum, setRandomAlbum] = useState({});
-  const [reviews, setReviews] = useState([]);
+    const [randomAlbum, setRandomAlbum] = useState({});
+    const [reviews, setReviews] = useState([]);
 
-  const altText = `a photo of ${randomAlbum.title}, an album by ${randomAlbum.artist}`;
+    const altText = `a photo of ${randomAlbum.title}, an album by ${randomAlbum.artist}`;
 
-  const getRandomAlbum = async () => {
-    try {
-      const response = await fetch(`/api/v1/albums/random`);
-      if (!response.ok) {
-        const errorMessage = `${response.status} (${response.statusText})`;
-        const error = new Error(errorMessage);
-        throw error;
-      }
-      const responseBody = await response.json();
-      setRandomAlbum(responseBody);
-      setReviews(responseBody.reviews);
-    } catch (err) {
-      console.error(`Error in Fetch: ${err.message}`);
-    }
-  };
+    const getRandomAlbum = async () => {
+        try {
+            const response = await fetch(`/api/v1/albums/random`);
+            if (!response.ok) {
+                const errorMessage = `${response.status} (${response.statusText})`;
+                const error = new Error(errorMessage);
+                throw error;
+            }
+            const responseBody = await response.json();
+            setRandomAlbum(responseBody);
+            setReviews(responseBody.reviews);
+        } catch (err) {
+            console.error(`Error in Fetch: ${err.message}`);
+        }
+    };
 
-  useEffect(() => {
-    getRandomAlbum();
-  }, []);
+    useEffect(() => {
+        getRandomAlbum();
+    }, []);
 
-  const reviewsList = reviews.map((review) => {
+    const reviewsList = reviews.map((review) => {
+        return (
+            <li key={review.id}>
+                <ReviewTile
+                    id={review.id}
+                    rating={review.rating}
+                    name={review.name}
+                    reviewBody={review.reviewBody}
+                    createdAt={review.createdAt}
+                />
+            </li>
+        );
+    });
+
     return (
-      <li key={review.id}>
-        <ReviewTile
-          id={review.id}
-          rating={review.rating}
-          name={review.name}
-          reviewBody={review.reviewBody}
-          createdAt={review.createdAt}
-        />
-      </li>
-    );
-  });
+        <div>
+            <div className="bgvideo">
+                <video loop muted autoPlay poster="img/videoframe.jpg" className="bgvideo">
+                    <source src="https://pitch-spork.s3.us-east-2.amazonaws.com/Main.mp4" type="video/mp4"/>
+                </video>
+            </div>
 
-  return (
-    <div className="grid-y medium-grid-frame">
-      <div className="cell shrink header medium-cell-block-container"></div>
-      <div className="cell medium-auto medium-cell-block-container">
-        <div className="grid-x grid-padding-x">
-          <div className="cell medium-4 medium-cell-block-y">
-            <h2>Try Something New From Our Collection!</h2>
-            <h1>{randomAlbum.title}</h1>
-            <img src={randomAlbum.coverUrl} />
-            <h3>{randomAlbum.artist}</h3>
-          </div>
-          <div className="cell medium-8 medium-cell-block-y">
-            <ul>{reviewsList}</ul>
-          </div>
+            <div>
+                <h2>Try Something New From Our Collection!</h2>
+                <h1>{randomAlbum.title}</h1>
+                <img src={randomAlbum.coverUrl}/>
+                <h3>{randomAlbum.artist}</h3>
+            </div>
+
+            <div>
+                <ul>{reviewsList}</ul>
+            </div>
+
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Landing;
