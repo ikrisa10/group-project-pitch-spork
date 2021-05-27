21615e698d09850aecc5166dfd4b2bd5e2ee4b8d
yousif Alasfar <yousif.alasfar@gmail.com>
2021-05-27 12:14:25 -0700

    styleling to index and undiscovered and album tile

--- src/main/frontend/components/AlbumTile.js
+++ src/main/frontend/components/AlbumTile.js
@@ -7,16 +7,17 @@ const AlbumTile = props => {
 
     const altText = `a photo of ${props.title}, an album by ${props.artist}`
     return (
-        <div className="inner-content cell small-3 spaceddown">
+        <div className="inner-content cell small-3 spaceddown" className="album-tile">
             <h3 className="namebox">{props.name}</h3>
             <Link to={`/albums/${id}`}>
                 <img className="indexImage" src={props.coverUrl} alt={altText} />
             </Link>
-            <div>
-                <h4>{props.title}, by {props.artist}</h4>
-            </div>
-            <div>
-                <h6>Genre: {props.genre}</h6>
+            <div className="description">
+                <ul>
+                <li><h4 className="album-title">{props.title} </h4></li>
+                <li><h4 className="album-d">By: {props.artist} </h4></li>
+                <li><h6 className="album-d">Genre: {props.genre} </h6></li>
+                </ul>
             </div>
         </div>
     )
